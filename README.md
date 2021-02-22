4669f4c168775afe49e5f744e6cb9099dd78ba6b
Abbey Sparrow <abbey@khrome.net>
2021-02-21 20:31:07 -0700

    only working locally

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-#npm-auto-link
+module-auto-link
+================
 
 NPM5 has completely screwed `npm link` development for multi-module developers who need to develop 1 feature among more than one package without craziness.
 
 This just opens your `package.json`, looks at the dependencies and then links to any directories it finds in the parent that match your dependencies.
+
+You can do it without installing using: `npx module-auto-link -c 'npm-auto-link'`
 
 now you can just execute `npm-auto-link` and not continually downgrading or playing the relink dance.
 
--- fix.js
+++ fix.js
@@ -1,28 +1,6 @@
 #!/usr/bin/env node
-var fs = require('fs');
 var pack = require(process.cwd()+'/package');
-var npm = require('npm');
-var arrays = require('async-arrays');
-var intersect = require('array-intersection');
-
-var packages = [];
-if(pack.dependencies)
-    packages = packages.concat(Object.keys(pack.dependencies));
-if(pack.peerDependencies)
-    packages = packages.concat(Object.keys(pack.peerDependencies));
-if(pack.devDependencies)
-    packages = packages.concat(Object.keys(pack.devDependencies));
-if(pack.optionalDependencies)
-    packages = packages.concat(Object.keys(pack.optionalDependencies));
-fs.readdir(process.cwd()+'/..', function(err, files){
-    var linkables = intersect(files, packages);
-    npm.load({
-        loaded: false
-    }, function (err) {
-        arrays.forEachEmission(linkables, function(packageName, index, done){
-            npm.commands.link(packageName, done)
-        }, function(){
-            console.log('linked: '+linkables.join(', '));
-        });
-    });
+var link = require('./linker');
+link(pack, function(err, linked){
+    console.log('linked: '+linkables.join(', '));
 });
